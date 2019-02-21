17020a9bb41f1d6a0631be11fbf117334395e396
Trang <trangmaiq@gmail.com>
2019-02-21 14:59:09 +0700

    Update UploadOptions: Sort all the parameters in alphabetical order

--- cloudinary/upload.go
+++ cloudinary/upload.go
@@ -27,59 +27,68 @@ type UploadRequest struct {
 }
 
 type UploadOptions struct {
-	// Upload Preset is required for unsigned uploading and
-	// optional for signed uploading
-	UploadPreset *string `json:"upload_preset,omitempty"`
-
-	// Optional fields to call upload request
-	// Naming and storage fields
-	PublicId       *string `json:"public_id,omitempty"`
-	Folder         *string `json:"folder,omitempty"`
-	UseFilename    *bool   `json:"use_filename,omitempty"`
-	UniqueFilename *bool   `json:"unique_filename,omitempty"`
-	ResourceType   *string `json:"resource_type,omitempty"`
-	Type           *string `json:"type,omitempty"`
+	Timestamp *string `json:"timestamp,omitempty"`
 	//AccessControl           interface{} `json:"access_control,omitempty"`
-	AccessMode              *string `json:"access_mode,omitempty"`
-	DiscardOriginalFilename *bool   `json:"discard_original_filename,omitempty"`
-	Overwrite               *bool   `json:"overwrite,omitempty"`
-
-	// Resource data fields
-	Tags            *string `json:"tags,omitempty"`
-	Context         *string `json:"context,omitempty"`
-	Colors          *bool   `json:"colors,omitempty"`
-	Faces           *bool   `json:"faces,omitempty"`
-	QualityAnalysis *bool   `json:"quality_analysis,omitempty"`
-	ImageMetadata   *bool   `json:"image_metadata,omitempty"`
-	Phash           *bool   `json:"phash,omitempty"`
-	//ResponsiveBreakpoints interface{} `json:"responsive_breakpoints,omitempty"`
+	AccessMode     *string  `json:"access_mode,omitempty"`
+	AllowedFormats *string  `json:"allowed_formats,omitempty"`
+	Async          *bool    `json:"async,omitempty"`
 	AutoTagging    *float64 `json:"auto_tagging,omitempty"`
-	Categorization *string  `json:"categorization,omitempty"`
-	Detection      *string  `json:"detection,omitempty"`
-	OCR            *string  `json:"ocr,omitempty"`
-	Exif           *bool    `json:"exif,omitempty"`
 
-	// Manipulations fields
+	BackgroundRemoval *string `json:"background_removal,omitempty"`
+	Backup            *bool   `json:"backup,omitempty"`
+
+	Callback          *string `json:"callback,omitempty"`
+	Categorization    *string `json:"categorization,omitempty"`
+	Colors            *bool   `json:"colors,omitempty"`
+	Context           *string `json:"context,omitempty"`
+	CustomCoordinates *string `json:"custom_coordinates,omitempty"`
+
+	Detection               *string `json:"detection,omitempty"`
+	DiscardOriginalFilename *bool   `json:"discard_original_filename,omitempty"`
+
 	Eager                *string `json:"eager,omitempty"`
 	EagerAsync           *bool   `json:"eager_async,omitempty"`
 	EagerNotificationURL *string `json:"eager_notification_url,omitempty"`
-	Transformation       *string `json:"transformation,omitempty"`
-	Format               *string `json:"format,omitempty"`
-	CustomCoordinates    *string `json:"custom_coordinates,omitempty"`
-	FaceCoordinates      *string `json:"face_coordinates,omitempty"`
-	BackgroundRemoval    *string `json:"background_removal,omitempty"`
-	RawConvert           *string `json:"raw_convert,omitempty"`
+	Exif                 *bool   `json:"exif,omitempty"`
 
-	// Additional options fields
-	AllowedFormats    *string `json:"allowed_formats,omitempty"`
-	Async             *bool   `json:"async,omitempty"`
-	Backup            *bool   `json:"backup,omitempty"`
-	Callback          *string `json:"callback,omitempty"`
-	Headers           *string `json:"headers,omitempty"`
-	Invalidate        *bool   `json:"invalidate,omitempty"`
-	Moderation        *string `json:"moderation,omitempty"`
-	Proxy             *string `json:"proxy,omitempty"`
+	FaceCoordinates *string `json:"face_coordinates,omitempty"`
+	Faces           *bool   `json:"faces,omitempty"`
+	Folder          *string `json:"folder,omitempty"`
+	Format          *string `json:"format,omitempty"`
+
+	Headers *string `json:"headers,omitempty"`
+
+	ImageMetadata *bool `json:"image_metadata,omitempty"`
+	Invalidate    *bool `json:"invalidate,omitempty"`
+
+	Moderation *string `json:"moderation,omitempty"`
+
+	NotificationURL *string `json:"notification_url,omitempty"`
+
+	OCR       *string `json:"ocr,omitempty"`
+	Overwrite *bool   `json:"overwrite,omitempty"`
+
+	Phash    *bool   `json:"phash,omitempty"`
+	Proxy    *string `json:"proxy,omitempty"`
+	PublicId *string `json:"public_id,omitempty"`
+
+	QualityAnalysis *bool `json:"quality_analysis,omitempty"`
+
+	RawConvert        *string `json:"raw_convert,omitempty"`
 	ReturnDeleteToken *bool   `json:"return_delete_token,omitempty"`
+
+	Tags           *string `json:"tags,omitempty"`
+	Transformation *string `json:"transformation,omitempty"`
+	Type           *string `json:"type,omitempty"`
+
+	UniqueFilename *bool `json:"unique_filename,omitempty"`
+	// Upload Preset is required for unsigned uploading and
+	// optional for signed uploading
+	UploadPreset *string `json:"upload_preset,omitempty"`
+	UseFilename  *bool   `json:"use_filename,omitempty"`
+
+	ResourceType *string `json:"resource_type,omitempty"`
+	//ResponsiveBreakpoints interface{} `json:"responsive_breakpoints,omitempty"`
 
 	isUnsignedUpload bool `json:"-"`
 }
